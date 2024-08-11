2d8672b803450afaee144b9194942ba5b33bb1e7
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-08-11 22:54:00 +0530

    create organization class

---
+++ src/organization/organization.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { OrganizationController } from "./organization.controller";
+import { OrganizationService } from "./organization.service";
+
+@Module({
+    imports:[],
+    controllers:[OrganizationController],
+    providers:[OrganizationService]
+})
+
+export class OrganizationModule{}
