6e47cd762e993ca106133f353309ac2ef9f763cf
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-08-27 00:06:01 +0530

    Authentication (#26)
    * implement mail service
    
    * verification link implemented
    
    * complete authentication and organization onboarding process
    
    * fix auth token issue
    
    * teacher schema, city search
    
    * create common schema module and remove un-used files
    
    * remove un-used file
    
    * resolve conflicts
    
    * add formcontrol

--- src/core/schemas/branch.schema.ts
+++ src/core/schemas/branch.schema.ts
@@ -11,28 +11,43 @@ export class Branch extends BaseSchema {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true })
   organizationId: Organization;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, formControl: { name: 'input', label: 'Branch Name', required: true } })
   name: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, formControl: { name: 'input', label: 'Address', required: true } })
   address: string;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Country', required: true })
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Country',
+    required: true,
+    formControl: { name: 'select', label: 'Country', required: true }
+  })
   countryId: Country;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'State', required: true })
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'State',
+    required: true,
+    formControl: { name: 'select', label: 'State', required: true }
+  })
   stateId: State;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true })
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'City',
+    required: true,
+    formControl: { name: 'select', label: 'City', required: true }
+  })
   cityId: City;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, formControl: { name: 'input', label: 'Pincode', required: true } })
   pincode: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, formControl: { name: 'input', label: 'Email', required: true } })
   email: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, formControl: { name: 'input', label: 'Phone', required: true } })
   phone: string;
 
   @Prop({ type: Boolean, default: true })
