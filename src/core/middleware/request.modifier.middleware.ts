0509b11874de5bb2eeae9c481d723e0bc56bf1e3
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-08-28 08:21:47 +0530

    Authentication (#28)
    * implement mail service
    
    * verification link implemented
    
    * complete authentication and organization onboarding process
    
    * fix auth token issue
    
    * teacher schema, city search
    
    * create common schema module and remove un-used files
    
    * remove un-used file
    
    * resolve conflicts
    
    * add formcontrol
    
    * update schema, handle jwt token, working on middleware
    
    * create teacher and student enrollment schema

--- src/core/middleware/request.modifier.middleware.ts
+++ src/core/middleware/request.modifier.middleware.ts
@@ -3,6 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class RequestModifierMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    console.log("Req user: ", req.user);
     next();
   }
 }
--- src/core/schemas/teacher-enrollment.schema.ts
+++ src/core/schemas/teacher-enrollment.schema.ts
@@ -4,15 +4,18 @@ import { BaseSchema, BaseSchemaOptions } from './base.schema';
 import { Student } from './student.schema';
 import { StudentRoutes } from 'src/students/student.routes';
 import { Class } from './class.schema';
+import { Branch } from './branch.schema';
+import { Teacher } from './teacher.schema';
+import { TeacherRoutes } from 'src/teachers/teacher.routes';
 
 @BaseSchemaOptions()
-export class Enrollment extends BaseSchema {
+export class TeacherEnrollment extends BaseSchema {
 
-  @Prop({ type: Types.ObjectId, ref: 'Branch', required: true })
-  branchId: Types.ObjectId;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true })
+  branchId: Branch;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true, formControl: { name: 'autosuggest', label: "Select Student", required: true, apiDetails: { endpoint: StudentRoutes.FETCH_STUDENT, onMount: true } } })
-  studentId: Student;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: true, formControl: { name: 'autosuggest', label: "Select Teacher", required: true, apiDetails: { endpoint: TeacherRoutes.FETCH_TEACHER, onMount: true } } })
+  studentId: Teacher;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true,
     formControl: { name: 'autosuggest', label: 'Select Class', required: true }
@@ -23,4 +26,4 @@ export class Enrollment extends BaseSchema {
   enrollmentDate: Date;
 }
 
-export const EnrollmentSchema = SchemaFactory.createForClass(Enrollment);
+export const TeacherEnrollmentSchema = SchemaFactory.createForClass(TeacherEnrollment);
