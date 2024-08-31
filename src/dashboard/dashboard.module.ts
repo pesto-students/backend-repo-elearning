c69526c46989d23448caac8d04245d04eb71964a
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-08-31 22:33:47 +0530

    resolve dependency error

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { TeacherModule } from 'src/teachers/teacher.module';
+import { StudentModule } from 'src/students/student.module';
+import { OnlineClassModule } from 'src/online-class/online-class.module';
+import { ClassModule } from 'src/classes/class.module';
+import { BranchModule } from 'src/branch/branch.module';
 
 @Module({
-  imports:[],
+  imports:[
+    TeacherModule,
+    StudentModule,
+    OnlineClassModule,
+    ClassModule,
+    BranchModule
+  ],
   providers: [DashboardService],
   controllers: [DashboardController]
 })
