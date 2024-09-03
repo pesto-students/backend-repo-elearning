a18c2c6651d99be9dd7542d4fa9151887eef81c2
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-09-04 00:50:49 +0530

    upsert chat in single api

--- src/module-management/repository/module-management.repository.ts
+++ src/module-management/repository/module-management.repository.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { Modules } from "src/core/schemas/modules/modules.schema";
 import { OrganizationModuleSettings } from "src/core/schemas/modules/organization-module-settings.schema";
 
 @Injectable()
 export class ModuleManagementRepository {
     constructor(
-        @InjectModel(OrganizationModuleSettings.name) private orgModuleSettingsModel: Model<OrganizationModuleSettings>
+        @InjectModel(OrganizationModuleSettings.name) private orgModuleSettingsModel: Model<OrganizationModuleSettings>,
+        @InjectModel(Modules.name) private readonly modulesModel: Model<Modules>,
     ) { }
 
     async subscribeModule(subscriptionData) {
@@ -35,5 +37,24 @@ export class ModuleManagementRepository {
           .exec();
     
         return !!organizationModule;
+      }
+
+      async getModulesWithSubscription(organizationId: string, branchId: string): Promise<any[]> {
+        // Fetch all modules from the database
+        const allModules = await this.modulesModel.find().exec();
+        
+        // Fetch the organization’s subscribed modules for a specific branch
+        const subscribedModules = await this.orgModuleSettingsModel
+          .find({ organizationId, branchId, enabled: true })
+          .exec();
+      
+        // Get the list of subscribed module IDs
+        const subscribedModuleIds = subscribedModules.map((module) => module.moduleId.toString());
+      
+        // Combine the data: map all modules and add a 'isSubscribed' flag
+        return allModules.map((module) => ({
+          ...module.toObject(),
+          isSubscribed: subscribedModuleIds.includes(module._id.toString()), // Check if the module is subscribed
+        }));
       }
 }
